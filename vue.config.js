db2a737d20218622b10298c09d503e98951e4265
ankitjain0748 <ankitjain0748@gmail.com>
2025-11-08 19:19:31 +0530

    aa

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
+// vue.config.js
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: './',
-  outputDir: 'dist'
+  outputDir: 'dist',
+  assetsDir: 'assets'
 })
